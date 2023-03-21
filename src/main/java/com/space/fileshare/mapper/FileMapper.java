026f429d04809947037c2a280f1c298a597c863d
vague <jmzhao1996@icloud.com>
2023-03-21 11:35:50 +0800

    update

---
+++ src/main/java/com/space/fileshare/mapper/FileMapper.java
@@ -0,0 +1,13 @@
+package com.space.fileshare.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.space.fileshare.vo.FileVo;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author vague 2022/4/29 17:00
+ */
+@Mapper
+public interface FileMapper extends BaseMapper<FileVo> {
+
+}
